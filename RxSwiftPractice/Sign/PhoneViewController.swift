490d889a2a05d722531268b94dd0ff3c705d9feb
홍정민 <dream7739@naver.com>
2024-08-01 21:22:48 +0900

    phoneVC 구현

---
+++ RxSwiftPractice/Sign/PhoneViewController.swift
@@ -0,0 +1,88 @@
+//
+//  PhoneViewController.swift
+//  RxSwiftPractice
+//
+//  Created by 홍정민 on 8/1/24.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+class PhoneViewController: UIViewController {
+   
+    let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
+    let descriptionLabel = UILabel()
+    let nextButton = PointButton(title: "다음")
+
+    let phoneData = BehaviorRelay(value: "010")
+    
+    let disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = Color.white
+        
+        configureLayout()
+        
+        bind()
+    }
+    
+    func bind(){
+        phoneData
+            .bind(to: phoneTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        phoneTextField.rx.text
+            .orEmpty
+            .map { $0.count >= 10}
+            .bind(with: self) { owner, value in
+                let color: UIColor = value ? .black : .gray
+                owner.phoneTextField.layer.borderColor = color.cgColor
+                owner.nextButton.backgroundColor = color
+                owner.nextButton.isEnabled = value
+                owner.descriptionLabel.isHidden = value
+                
+            }
+            .disposed(by: disposeBag)
+        
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.pushViewController(BirthdayViewController(), animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+    }
+    
+    
+    func configureLayout() {
+        view.addSubview(phoneTextField)
+        view.addSubview(nextButton)
+        view.addSubview(descriptionLabel)
+         
+        phoneTextField.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(phoneTextField.snp.bottom).offset(4)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(phoneTextField.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        descriptionLabel.text = "전화번호는 10자리 이상이어야 합니다"
+
+        
+    }
+
+}
+
